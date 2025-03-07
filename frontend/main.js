3c48bb5c66e993481fe3e395d31a5dcf9fc3bae4
aramiscal <aron-ramiscal@uiowa.edu>
2025-03-06 19:32:52 -0600

    Update index.html

--- frontend/main.js
+++ frontend/main.js
@@ -1,32 +1,32 @@
-const api = 'http://127.0.0.1:8000/List';
+const api = "http://127.0.0.1:8000";
 
 const displayList = (list) => {
-    const tbody = document.getElementById('list-rows');
-    tbody.innerHTML = '';
-    const rows = list.map((x) => {
-        return `<tr>
+  const tbody = document.getElementById("list-rows");
+  tbody.innerHTML = "";
+  const rows = list.map((x) => {
+    return `<tr>
             <td>${x.id}</td>
             <td>${x.name}</td>
             <td>${x.quantity}</td>
         </tr>`;
-    });
-    tbody.innerHTML = rows.join(' ');
-}
+  });
+  tbody.innerHTML = rows.join(" ");
+};
 
 const getList = () => {
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = () => {
-        if(xhr.readyState == 4 && xhr.status == 200) {
-            data = JSON.parse(xhr.responseText);
-            console.log(data);
-            displayList(data);
-        }
-    };
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      data = JSON.parse(xhr.responseText);
+      console.log(data);
+      displayTodos(data);
+    }
+  };
 
-    xhr.open('GET', api, true);
-    xhr.send();
+  xhr.open("GET", api, true);
+  xhr.send();
 };
 
 (() => {
-    getList();
+  getList();
 })();
