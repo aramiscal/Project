741924cb698cc7390d7f8ed1a5e28b5d013632e6
aramiscal <aron-ramiscal@uiowa.edu>
2025-04-15 13:38:41 -0500

    More Errors

--- api/routers/list_routes.py
+++ api/routers/list_routes.py
@@ -20,11 +20,11 @@ async def add_item(list: ListRequest) -> Item:
 
 @list_router.delete("/{name}")
 async def delete_item_by_name(name: str) -> dict:
-    for i in range(len(full_list)):
-        if full_list[i].name == name:
-            full_list.pop(i)
-            return {"msg": f"The item with Name = {name} is removed"}
-        
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND, detail=f"Item with Name={name} is not found"
-    )
+    item = await Item.find_one(Item.name == name) 
+    if item:
+        await item.delete()
+        return {"msg": f"The item with Name = {name} is removed"}
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"Item with Name={name} is not found"
+        )
